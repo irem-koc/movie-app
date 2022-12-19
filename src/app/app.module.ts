e9a188de8bad964a7c37ffb91ef99af36157656f
irem-koc <irem_koc_75@hotmail.com>
2022-12-19 20:01:55 +0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule //
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //starter component
 })
 export class AppModule { }
